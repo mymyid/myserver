67f8859833643026ff1f2b681b8177a2d818ebf7
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-25 18:27:54 +0700

    packaging

--- main.go
+++ main.go
@@ -4,77 +4,12 @@ import (
 	"log"
 
 	"github.com/domyid/chatserver/config"
+	"github.com/domyid/chatserver/helper/chatroot"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 )
-
-type client struct{} // Add more data to this type if needed
-
-var clients = make(map[*websocket.Conn]client) // Note: although large maps with pointer-like types (e.g. strings) as keys are slow, using pointers themselves as keys is acceptable and fast
-var register = make(chan *websocket.Conn)
-var broadcast = make(chan string)
-var unregister = make(chan *websocket.Conn)
-
-func runHub() {
-	for {
-		select {
-		case connection := <-register:
-			clients[connection] = client{}
-			log.Println("connection registered")
-
-		case message := <-broadcast:
-			log.Println("message received:", message)
-
-			// Send the message to all clients
-			for connection := range clients {
-				if err := connection.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
-					log.Println("write error:", err)
-
-					unregister <- connection
-					connection.WriteMessage(websocket.CloseMessage, []byte{})
-					connection.Close()
-				}
-			}
-
-		case connection := <-unregister:
-			// Remove the client from the hub
-			delete(clients, connection)
-
-			log.Println("connection unregistered")
-		}
-	}
-}
-
-func runSocket(c *websocket.Conn) {
-	// When the function returns, unregister the client and close the connection
-	defer func() {
-		unregister <- c
-		c.Close()
-	}()
-
-	// Register the client
-	register <- c
-
-	for {
-		messageType, message, err := c.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Println("read error:", err)
-			}
-
-			return // Calls the deferred function, i.e. closes the connection on error
-		}
-
-		if messageType == websocket.TextMessage {
-			// Broadcast the received message
-			broadcast <- string(message)
-		} else {
-			log.Println("websocket message received of type", messageType)
-		}
-	}
-}
 
 func main() {
 	site := fiber.New(config.Iteung)
@@ -84,9 +19,9 @@ func main() {
 
 	site.Static("/", "./home.html")
 
-	go runHub()
+	go chatroot.RunHub()
 
-	site.Get("/ws", websocket.New(runSocket))
+	site.Get("/ws", websocket.New(chatroot.RunSocket))
 
 	log.Fatal(site.Listen(config.IPPort))
 }
